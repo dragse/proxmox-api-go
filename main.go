1858439dc85aebca02d6384b630f6fbe45b898ba
lbrinkhaus <lennard.brinkhaus@dragse.de>
2021-10-31 00:26:16 +0200

    Create some Endpoints to proxmox

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/dragse/proxmox-api-go/client"
+	"github.com/dragse/proxmox-api-go/static"
 	"log"
 )
 
@@ -9,7 +11,7 @@ func main() {
 	hist := client.ProxmoxSession{
 		Hostname:  "192.168.1.205:8006",
 		Username:  "prox-api@pve!test-token",
-		Token:  "1fedfb41-b8f3-40a7-8707-6f40fe617d19",
+		Token:     "1fedfb41-b8f3-40a7-8707-6f40fe617d19",
 		VerifySSL: false,
 	}
 
@@ -26,4 +28,11 @@ func main() {
 	}
 
 	log.Println("Login Success")
+
+	content, err := hist.Get(static.EndpointNodes)
+
+	m := content.Data
+	test, _ := json.Marshal(m)
+	log.Println(string(test))
+
 }
--- proxmox/cluster.go
+++ proxmox/cluster.go
@@ -6,20 +6,21 @@ type ProxmoxCluster struct {
 	sessions []*client.ProxmoxSession
 
 	Cluster *ClusterInformation
+	Nodes   []*NodeInformation
 }
 
 type ClusterInformation struct {
-	Name string
-	Nodes int
-	Quorate int
-	Version int
+	Name    string `json:"name"`
+	Nodes   int    `json:"nodes"`
+	Quorate int    `json:"quorate"`
+	Version int    `json:"version"`
 }
 
 type NodeInformation struct {
-	IP string
-	Level string
-	Local int
-	Name string
-	NodeID int
-	Online bool
+	IP     string `json:"ip"`
+	Level  string `json:"level"`
+	Local  int    `json:"local"`
+	Name   string `json:"name"`
+	NodeID int    `json:"node_id"`
+	Online bool   `json:"online"`
 }
